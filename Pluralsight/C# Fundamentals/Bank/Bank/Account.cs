aa8b575025643c1ef739d9618ad34db7958d218a
DESKTOP-N5GFK26\Daniel <GameDeveloperUE67@gmail.com>
2026-08-04 12:16:28 +0200

    Bank app assignment in C#

---
+++ Pluralsight/C# Fundamentals/Bank/Bank/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    internal class Account : Bank
+    {
+      public  string FirstName { get; private set; }
+      public  string LastName { get; private set; }
+      public string Email { get; private set; }
+      private int AccountNumber { get; set; }
+      private double Balance { get; set; }
+
+        private Account() 
+        {
+            Accounts++;
+        }
+
+        public Account(string firstname, string lastname, int accountNumber, double balance): this()
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public Account(string firstname, string lastname, int accountnumber, double balance, string email) : this(firstname, lastname, accountnumber, balance)
+        {
+            Email = email;
+        }
+    }
+}
---
+++ Pluralsight/C# Fundamentals/Bank/Bank/Bank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    internal class Bank
+    {
+        public const string Name = "Bank of America";
+
+        private protected int Accounts { get; set; } 
+
+
+
+
+    }
+}
