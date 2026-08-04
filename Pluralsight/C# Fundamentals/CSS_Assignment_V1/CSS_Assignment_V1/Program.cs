40119105354e12fd99f50004a2390e6e4059e846
DESKTOP-N5GFK26\Daniel <GameDeveloperUE67@gmail.com>
2026-08-04 11:56:57 +0200

    Css assignment version 1

---
+++ Pluralsight/C# Fundamentals/CSS_Assignment_V1/CSS_Assignment_V1/Program.cs
@@ -0,0 +1,16 @@
+namespace CSS_Assignment_V1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            app.UseFileServer();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
