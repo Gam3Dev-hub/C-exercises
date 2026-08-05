d8bb78c68299ab0f84684c74f48a79035b76633c
DESKTOP-N5GFK26\Daniel <GameDeveloperUE67@gmail.com>
2026-08-05 16:03:24 +0200

    Async/Await

---
+++ Pluralsight/C# Fundamentals/AsyncAwait/AsyncAwait/OrangeJuice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Breakfast
+{
+    internal class OrangeJuice
+    {
+        public OrangeJuice() { }
+
+        public static OrangeJuice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new OrangeJuice();
+        } 
+    }
+}
---
+++ Pluralsight/C# Fundamentals/AsyncAwait/AsyncAwait/Program.cs
@@ -0,0 +1,19 @@
+﻿global using static System.Console;
+using Breakfast;
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Coffee.PourCoffee();
+            Egg.FryEggs(2);
+            HashBrown.FryHashBrowns(3);
+            Toast.ToastBread(2);
+            Toast.ApplyJam();
+            Toast.ApplyButter();
+            
+            ReadKey();
+        }
+    }
+}
